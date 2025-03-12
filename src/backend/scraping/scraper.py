92cf1fa5337d1ab204724a1409dd2b6474cf1ff2
slyeet03 <shobhit7419@gmail.com>
2025-03-12 15:53:59 +0530

    added future shit to do

--- src/backend/scraping/scraper.py
+++ src/backend/scraping/scraper.py
@@ -9,6 +9,8 @@ query = query_prompt.prompt_to_query()
 driver = util.openBrowser()
 
 # TODO: Add multithreading for scrapers
+# TODO: Scrape more sites: urbanic, rare rabbit, westside, fab india, bombay palsey, no nasties, turms
+# TODO: MAYBE and a big MAYBE add a whole ass thrift store
 a = util.scrape("amazon", query, driver)
 time.sleep(2)
 b = util.scrape("myntra", query, driver)
